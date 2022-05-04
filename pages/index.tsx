cb01134bcaa4fbaf68f2dc17f0186abc3ad62bd2
Kentaro Suzuki <kentaro84207@gmail.com>
2022-05-04 18:47:06 +0900

    init

---
+++ pages/index.tsx
@@ -0,0 +1,46 @@
+import Container from '../components/container'
+import MoreStories from '../components/more-posts'
+import Intro from '../components/intro'
+import Layout from '../components/layout'
+import { getAllPosts } from '../lib/api'
+import Head from 'next/head'
+import { CMS_NAME } from '../lib/constants'
+import Post from '../types/post'
+
+type Props = {
+  allPosts: Post[]
+}
+
+const Index = ({ allPosts }: Props) => {
+  const morePosts = allPosts.slice(1)
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title>ken-dev-blog</title>
+        </Head>
+        <Container>
+          <Intro />
+          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
+        </Container>
+      </Layout>
+    </>
+  )
+}
+
+export default Index
+
+export const getStaticProps = async () => {
+  const allPosts = getAllPosts([
+    'title',
+    'date',
+    'slug',
+    'author',
+    'coverImage',
+    'excerpt',
+  ])
+
+  return {
+    props: { allPosts },
+  }
+}
