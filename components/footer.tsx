027a734ee6c523121ed0cc7227645086befc934f
Kentaro Suzuki <kentaro84207@gmail.com>
2022-05-08 10:45:07 +0900

    fix footer link

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,23 @@
-import Link from 'next/link'
 import Image from 'next/image'
 
 const Footer = () => {
   return (
     <footer className="py-6">
       <div className="flex items-center justify-center gap-4">
-        <Link href="/">
-          <a>
-            <Image src="/icon/twitter.svg" alt="twitter" height={30} width={30} />
-          </a>
-        </Link>
-        <Link href="/">
-          <a>
-            <Image src="/icon/github.svg" alt="github" height={30} width={30} />
-          </a>
-        </Link>
+        <a
+          href="https://twitter.com/kentaro_s90"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/icon/twitter.svg" alt="twitter" height={30} width={30} />
+        </a>
+        <a
+          href="https://github.com/kentaro84207"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image src="/icon/github.svg" alt="github" height={30} width={30} />
+        </a>
       </div>
     </footer>
   )
